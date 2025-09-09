a7e7b5d43fb51986de8b9b3a0a960b640a0fccdc
Jose F. Tinoco Fokkelman <joseftf1991@gmail.com>
2025-09-09 17:48:08 +0200

    actu09

--- ej7/index.php
+++ ej7/index.php
@@ -8,6 +8,7 @@
         if(($numero1 > $numero2) && ($numero1 > $numero3)){
             echo "$numero1 es el mayor";
             elseif($numero1 < $numero2) {
+                echo "";
 
         }
 }
